da141f5a9a0e7ec998ec3de6f2c4abf11f0d484d
devalto <51037255+dev-alto@users.noreply.github.com>
2026-07-17 04:06:41 -0700

    feat: implement of initial landing page/home design

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,56 @@
-import Image from "next/image";
+import Image from 'next/image'
+
+import { MediaShelf } from './components/MediaShelf'
+import { ScreenLoading } from './components/ScreenLoading'
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
+    <div className="">
+      {/* <ScreenLoading /> */}
+
+      <main className="flex flex-col items-center gap-8">
+
+        {/* hero */}
+        <section className="w-full h-[calc(100vh-64px)] py-12 bg-backdrop text-white">
+          <div className="sm:mx-16 h-full bg-neutral-400 sm:rounded-2xl overflow-hidden">
+
+            {/* namemark and slideshow/video */}
+            <section className="group relative w-full h-[60%] flex flex-col items-center justify-center bg-black sm:rounded-xl overflow-hidden">
+              <video className="z-10 absolute rotate-90 w-full aspect-square object-bottom opacity-15 scale-110" style={{maskImage: 'linear-gradient(0deg,rgba(0, 0, 0, 1) 0%, rgba(0, 0, 0, 0) 60%)'}} src="/assets/backdrop.webm" autoPlay loop muted disablePictureInPicture></video>
+              <video className="z-10 absolute rotate-270 w-full aspect-square object-bottom opacity-15 scale-110" style={{maskImage: 'linear-gradient(0deg,rgba(0, 0, 0, 1) 0%, rgba(0, 0, 0, 0) 60%)'}} src="/assets/backdrop.webm" autoPlay loop muted disablePictureInPicture></video>
+              <video className="absolute w-full h-full object-cover opacity-75 blur-xs" src="/assets/WEKFESTFINAL_2.webm" autoPlay loop muted disablePictureInPicture></video>
+              <h3 className="z-30 translate-y-0 starting:translate-y-full opacity-100 starting:opacity-0 text-2xl font-bernoru ease-out duration-500 delay-200">WELCOME TO</h3>
+              <h1 className="z-30 pr-10 text-[140px] sm:text-[200px] italic font-conchin font-semibold tracking-tight leading-28 sm:leading-40 group-hover:drop-shadow-[0_6px_0_rgba(255,255,255,0.4)] transition-all duration-500">3TM</h1>
+            </section>
+
+            {/* instagram/recent media section */}
+            <MediaShelf />
+
+          </div>
+        </section>
+
+        <section className="w-full h-32 bg-purple-500">
+
+
+        </section>
+
+        <section className="w-full h-32 bg-orange-500">
+
+
+        </section>
+
+        <section className="w-full h-32 bg-sky-500">
+
+        </section>
+
+        <section className="w-full h-32 bg-lime-500">
+
+
+        </section>
+
+
+
       </main>
     </div>
-  );
+  )
 }
